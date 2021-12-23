cb63c067313551b5c864e6e0291aa797291e26a0
김봄비 <rlawldud627@naver.com>
2021-12-23 11:30:57 +0900

    Coin

--- src/App.js
+++ src/App.js
@@ -1,51 +1,50 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    // button에 기본으로 새로고침 되므로 버튼 누를때 새로고침 안되게
-    if (toDo === "") {
-      return;
-    }
-    // button(엔터) 되면 input 비워짐
-
-    setToDos((currentArray) => [toDo, ...currentArray]); 
-    // array를 직접적으로 수정하지 않으면서 setToDos로 array 에 element 추가하는 방법
-    
-    setToDo("");
-  }
-  // console.log(toDos);
-  
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  useEffect(() => {
+    fetch("http://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
   return (
     <div>
-      <h1>My To Do List ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-      <input
-        value={toDo}
-        onChange={onChange}
-        type="text"
-        placeholder="Write your to do!"
-      />
-      <button>+Add To De</button>
-      </form>
-      <hr />
-      <ul>
-        {toDos.map((item, index) => (
-          <li key={index}>{item}</li>
+      {/* <h1>The Coins! ({coins.length})</h1> */}
+      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
+      {loading ? (
+        <strong>Loading...</strong>
+       ) : ( 
+        <select>
+          {coins.map((coin) => (
+            <option>
+              {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD
+            </option>
+          ))}
+        </select>
+      )}
+      {/* <ul>
+        {coins.map((coin) => (
+          <li>
+            {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD
+          </li>
         ))}
-      </ul>
+      </ul> */}
+      {/* <select>
+        {coins.map((coin) => (
+          <li>
+            {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD
+          </li>
+        ))}
+      </select> */}
     </div>
   );
 }
-// key는 인덱스값(숫자) 0부터 시작하는 index값
+  
 
 export default App;
 
-
-// map(함수) 새로운 배열에(toDos의 배열 안 객체 수 만큼) 함수실행
-// item은 내가 지정한 이름 각 각의 item을 하나씩 함수실행해서 새배열에 넣음
-// ['there','are','you','are','how','hello'].map((item) => item.toUpperCase());
-// ['THERE','ARE','YOU','ARE','HOW','HELLO']
+// http://api.coinpaprika.com/v1/tickers
