64709319fad39deecdcbcad4fa051e01acc3734c
김봄비 <rlawldud627@naver.com>
2021-12-23 15:00:17 +0900

    Movie App part.2 react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,63 +1,24 @@
-import { useEffect, useState } from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import Detail from "./routes/Detail";
+import Home from "./routes/Home";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-  const getMovies = async() => {
-    // const response = await fetch(
-    //   `https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`
-    // );
-    // const json = await response.json();
-    // setMovies(json.data.movies);
-    // setLoading(false);
-      
-    const json = await (
-      await fetch(
-      `https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`
-      )
-    ).json();
-    setMovies(json.data.movies);
-    setLoading(false);
-  };
-  
-  useEffect(() => {
-    // fetch(
-    //   `https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`
-    // )
-    //   .then((response) => response.json())
-    //   .then((json) => {
-    //     setMovies(json.data.movies);
-    //     setLoading(false);
-    //   });
-    getMovies()
-  }, []);
-  // console.log(movies);
-
   return (
-    <div>
-      {loading ? (
-        <h1>Loading...</h1>
-      ) : (
-        <div>
-          {movies.map(movie => (
-            <div key={movie.id}>
-              <img src={movie.medium_cover_image} />
-              <h2>{movie.title}</h2>
-              <p>{movie.summary}</p>
-              <ul>
-                {movie.genres.map((g) => (
-                  <li key={g}>{g}</li>
-                ))}
-              </ul>
-            </div>
-          ))}
-        </div>
-      )}
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/movie">
+          <Detail />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
 export default App;
-
-// https://yts.mx/api
-// https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year
