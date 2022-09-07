964a65255b4bf7bb2b09757f1f576583820bd79b
bombi <bombic3@naver.com>
2022-09-07 14:21:05 +0900

    STYLE : 전체 style 수정

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -5,18 +5,23 @@ import styles from "./Movie.module.css";
 function Movie({ id, coverImg, title, year, summary, genres }) {
   return (
     <div className={styles.movie}>
-      <img src={coverImg} alt={title} className={styles.movie__img} />
-      <div>
-        <h2 className={styles.movie__title}>
-          <Link to={`/movie/${id}`}>{title}</Link>
-        </h2>
-        <h3 className={styles.movie__year}>{year}</h3>
-        <p>{summary.length > 235 ? `${summary.slice(0, 235)}...` : summary}</p>
-        <ul className={styles.movie__genres}>
-          {genres.map((g) => (
-            <li key={g}>{g}</li>
-          ))}
-        </ul>
+      <div className={styles.effect}>
+        <div className={styles.font}>
+          <img src={coverImg} alt={title} className={styles.movie__img} />
+        </div>
+        <span className={styles.back}>
+          <h2 className={styles.movie__title}>
+            <Link to={`/movie/${id}`}>{title}</Link>
+          </h2>
+          <h3 className={styles.movie__year}>{year}</h3>
+          <p>{summary.length > 235 ? `${summary.slice(0, 235)}...` : summary}</p>
+          <ul className={styles.movie__genres}>
+            Genre : 
+            {genres.map((g) => (
+              <li key={g}>'{g}'</li>
+            ))}
+          </ul>
+        </span>
       </div>
     </div>
   );
